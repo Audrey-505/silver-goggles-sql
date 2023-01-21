e54a73bc6c18caaefaca40df73cfbbbb26e96f52
Audrey Alcantara <adrianaquin22@gmail.com>
2023-01-21 11:07:34 -0500

    added get requests for each table

--- Main/server.js
+++ Main/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const mysql = require('mysql2')
+const inquirer = require('inquirer')
+const consoleTable = require('console.table')
+
 const PORT = process.env.PORT || 3001
 const app = express()
+
 
 //EXPRESS MIDDLEWARE 
 app.use(express.urlencoded({ extended: false }))
@@ -18,9 +22,33 @@ const db = mysql.createConnection(
     console.log('Connected to company_db')
 )
 
+//could I remove the 'AS deparment' from the sql line?
+app.get('/api/department', async (req, res) => {
+    try {
+        const result = await db.promise().query('SELECT id, department_name AS department FROM departments')
+        res.json(result[0])
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
+app.get('/api/role', async (req, res) => {
+    try {
+        const result = await db.promise().query('SELECT id, title, salary, department_id AS role FROM roles')
+        res.json(result[0])
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
-
+app.get('/api/employee', async (req, res) => {
+    try {
+        const result = await db.promise().query('SELECT id, first_name, last_name, role_id, manager AS employee FROM employees')
+        res.json(result[0])
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 
 
@@ -30,3 +58,16 @@ const db = mysql.createConnection(
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
+
+
+
+//EXAMPLE FOR BONUS DELETES:
+// app.delete('/api/department/:id', async (req, res) => {
+//     try {
+//         const result = await db.promise().query(`DELETE FROM departments WHERE id = ?`, [req.params.id])
+//         res.json(result[0])
+//     } catch (err) {
+//         res.status(500).json(err)
+//     }
+// })
+
